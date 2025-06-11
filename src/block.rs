f7b39e5a4e0270507a27b5248d2bd0d9615bf611
manishdotsqlite <manish.pokharel@proton.me>
2025-06-11 23:19:31 +0545

    hmm

--- src/block.rs
+++ src/block.rs
@@ -1,19 +1,19 @@
 use std::{fmt::{self, Debug, Formatter}, u64};
 
-use crate::{hashable::Hashable, library::{difficulty_bytes_as_u128, u128_bytes, u32_bytes, u64_bytes, BlockHash}};
+use crate::{hashable::Hashable, library::{difficulty_bytes_as_u128, u128_bytes, u32_bytes, u64_bytes, BlockHash}, transaction::Transaction};
 pub struct Block {
         pub index: u32,
         pub timestamp: u128,
         pub hash: BlockHash,
         pub prev_block_hash: BlockHash,
         pub nonce: u64,
-        pub payload: String,
+        pub transactions: Vec<Transaction>,
         pub difficulty: u128
 }
 
 impl Debug for Block {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "Block[{}]: {} at: {} with: {} nonce: {}", &self.index, &hex::encode(&self.hash), &self.timestamp, &self.payload, &self.nonce)
+        write!(f, "Block[{}]: {} at: {} with: {} nonce: {}", &self.index, &hex::encode(&self.hash), &self.timestamp, &self.transactions.len(), &self.nonce)
     }
 }
 
@@ -24,7 +24,7 @@ impl Hashable for Block {
                 bytes.extend(&u128_bytes(&self.timestamp));
                 bytes.extend(&self.prev_block_hash);
                 bytes.extend(&u64_bytes(&self.nonce));
-                bytes.extend(self.payload.as_bytes());
+                bytes.extend(self.transactions.iter().flat_map(|transaction| transaction.bytes()).collect::<Vec<u8>>());
                 bytes.extend(&u128_bytes(&self.difficulty));
 
                 bytes
@@ -32,8 +32,8 @@ impl Hashable for Block {
 }
 
 impl Block {
-    pub fn new(index: u32, timestamp: u128, prev_block_hash: BlockHash, nonce: u64, payload: String, difficulty: u128) -> Self {
-        Block { index, timestamp, hash: vec![0; 32], prev_block_hash, nonce, payload, difficulty }
+    pub fn new(index: u32, timestamp: u128, prev_block_hash: BlockHash,  transactions: Vec<Transaction>, difficulty: u128) -> Self {
+        Block { index, timestamp, hash: vec![0; 32], prev_block_hash, nonce: 0, transactions, difficulty }
     }
 
     pub fn mine(&mut self) {
--- src/main.rs
+++ src/main.rs
@@ -3,39 +3,40 @@ use blockchain::Blockchain;
 use hashable::Hashable;
 use library::now;
 
+use crate::transaction::Transaction;
+
 mod library;
 mod block;  
 mod hashable;
 mod blockchain;
+mod transaction;
 
 fn main() {
-
     let difficulty = 0x0000ffffffffffffffffffffffffffff;
 
-    let mut block = Block::new(0, now(), vec![0; 32], 0, "Genesis Block".to_owned(), difficulty);
+    let mut genesis_block = Block::new(0, now(), vec![0; 32], vec![
+        Transaction {inputs: vec![], outputs: vec![transaction::Output {value: 50, to_addr: "Alice".to_owned()}, transaction::Output {value: 7, to_addr: "Bob".to_owned() }]}
+    ], difficulty);
+
+    genesis_block.mine();
+    println!("Mined genesis block: {:?}", &genesis_block);
+
+    let mut last_hash = genesis_block.hash.clone();
+
+    let mut blockchain = Blockchain::new();
+    let _ = blockchain.update_with_block(genesis_block).expect("Failed to add genesis block.");
+
+     let mut block = Block::new(1, now(), last_hash, vec![
+        Transaction { inputs: vec![ ], outputs: vec![transaction::Output { to_addr: "Chris".to_owned(), value: 536 }] },
+        Transaction { inputs: vec![blockchain.blocks[0].transactions[0].outputs[0].clone()], outputs: vec![transaction::Output {to_addr: "Alice".to_owned(), value: 36}, transaction::Output {to_addr: "Bob".to_owned(), value: 12}]}
+    ], difficulty);
+
 
     block.mine();
-    println!("Mined Genesis Block: {:?}", block);
 
-    let mut last_hash = block.hash.clone();
+    println!("Mined block {:?}", &block);
 
-    let mut blockchain = Blockchain {
-        blocks: vec![block]
-    };
+    last_hash = block.hash.clone();
 
-    println!("Verify: {}", &blockchain.verify());
-
-    for i in 1..=10 {
-        let mut block = Block::new(i, now(), last_hash.clone(), 0, "Another Block".to_owned(), difficulty);
-
-        block.mine();
-        println!("{:?}", block);
-
-        last_hash = block.hash.clone();
-
-        blockchain.blocks.push(block);
-        println!("Verify: {}", &blockchain.verify());
-
-    }
-    
+    blockchain.update_with_block(block).expect("Failed to add block");
 }
